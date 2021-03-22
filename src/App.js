1dea5c508db2bc471177d5213c8ac388550a0767
Gumber <Gumber>
2021-03-22 17:51:29 -0400

    added in static map with mouseover events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { select, json, geoPath, geoNaturalEarth1 } from 'd3';
+import { feature } from 'topojson-client';
+import { useEffect } from 'react';
+
 
 function App() {
+  
+  const mouseOver = (e) => {
+    console.log('mouse overrrr');
+    console.log(e);
+  }
+
+  const mouseOut = (e) => {
+    console.log('mouse ouuuuuuuuut');
+    console.log(e);
+
+  }
+
+
+  useEffect(() => {
+
+    const svg = select('svg');
+    const projection = geoNaturalEarth1();
+    const pathGenerator = geoPath().projection(projection);
+
+    svg.append('path')
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({type: 'Sphere'}));
+    json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+    .then(data => {
+    const countries = feature(data, data.objects.countries);
+    svg.selectAll('path').data(countries.features)
+    .enter().append('path')
+      .attr('d', pathGenerator)
+      .attr('class', function(d) { return d.properties.name })
+      .attr('class', 'country')
+      .on("mouseover", mouseOver)
+      .on("mouseout", mouseOut);
+    });
+  }, []);  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <svg width="960" height="500"></svg>
+
+      </div>
     </div>
   );
 }
